5b13ec2e421da80a87288ddb5737abe7db61f04c
danglvh1004 <lvhd.hdlv@gmail.com>
2021-03-09 20:44:12 +0700

    Task 3: Send data from the main Activity to the second Activity

--- android-fundamentals-apps/TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ android-fundamentals-apps/TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -2,13 +2,23 @@ package com.example.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        textView = findViewById(R.id.text_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        textView.setText(message);
     }
 }
