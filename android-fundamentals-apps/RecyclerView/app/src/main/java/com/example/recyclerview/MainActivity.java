6937c6e6279140f906bcff40a6e41148ee6ac80d
danglvh1004 <lvhd.hdlv@gmail.com>
2021-03-10 10:48:13 +0700

    Task 2: Create a RecyclerView

--- android-fundamentals-apps/RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
+++ android-fundamentals-apps/RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
 
     private final LinkedList<String> mWordList = new LinkedList<>();
 
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,9 +36,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        for (int i = 0; i<20; i++){
+        for (int i = 0; i < 20; i++) {
             mWordList.addLast("Word " + i);
         }
+
+        // Get a handle to the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerview);
+        // Create an adapter and supply the data to be displayed.
+        mAdapter = new WordListAdapter(this, mWordList);
+        // Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+        // Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
