1037fb749a732fe70c35fe5b2de841cc5e5efa14
danglvh1004 <lvhd.hdlv@gmail.com>
2021-03-14 21:02:52 +0700

    Task 2: Implement the layout and MainActivity

--- android-fundamentals-apps/HelloCompat/app/src/main/java/com/example/hellocompat/MainActivity.java
+++ android-fundamentals-apps/HelloCompat/app/src/main/java/com/example/hellocompat/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.hellocompat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Text view for Hello World.
+    private TextView mHelloTextView;
+
+    // array of color names, these match the color resources in color.xml
+    private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
+            "indigo", "blue", "light_blue", "cyan", "teal", "green",
+            "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
+            "brown", "grey", "blue_grey", "black"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        // restore saved instance state (the text color)
+        if (savedInstanceState != null) {
+            mHelloTextView.setTextColor(savedInstanceState.getInt("color"));
+        }
+    }
+
+    public void initView() {
+        mHelloTextView = findViewById(R.id.hello_textview);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save the current text color
+        outState.putInt("color", mHelloTextView.getCurrentTextColor());
+    }
+
+    public void changeColor(View view) {
     }
 }
