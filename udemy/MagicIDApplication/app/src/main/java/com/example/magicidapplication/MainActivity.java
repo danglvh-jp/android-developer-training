683dd243746a6c23de417aed27a3d50ce8634027
danglvh1004 <lvhd.hdlv@gmail.com>
2021-03-14 08:52:00 +0700

    ID Application Part 2

--- udemy/MagicIDApplication/app/src/main/java/com/example/magicidapplication/MainActivity.java
+++ udemy/MagicIDApplication/app/src/main/java/com/example/magicidapplication/MainActivity.java
@@ -2,13 +2,79 @@ package com.example.magicidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etID;
+    Button btnSubmit;
+    TextView tvResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    public void initView() {
+        etID = findViewById(R.id.etID);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        tvResults = findViewById(R.id.tvResults);
+
+        tvResults.setVisibility(View.GONE);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String idNumber = etID.getText().toString().trim();
+
+                String dob = idNumber.substring(0, 6);
+
+                int gender = Integer.parseInt(Character.toString(idNumber.charAt(6)));
+
+                String sGender;
+
+                if (gender < 5) {
+                    sGender = getString(R.string.female);
+                } else {
+                    sGender = getString(R.string.male);
+                }
+
+                int nationality = Integer.parseInt(Character.toString(idNumber.charAt(10)));
+
+                String sNationality;
+
+                if (nationality == 0) {
+                    sNationality = getString(R.string.sacit);
+                } else {
+                    sNationality = getString(R.string.permanent);
+                }
+
+                tvResults.setText(getString(R.string.dob) + dob + "\n"
+                        + getString(R.string.gender) + sGender + "\n"
+                        + getString(R.string.nationality) + sNationality);
+
+                hideSoftKeyBoard();
+
+                tvResults.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    public void hideSoftKeyBoard() {
+        try {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        } catch (NullPointerException exception) {
+            exception.printStackTrace();
+        }
     }
 }
