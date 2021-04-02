02753cfea0e048188d6902d95518f324c2814441
danglvh1004 <lvhd.hdlv@gmail.com>
2021-04-02 09:25:26 +0700

    Task 1: Experiment with text entry attributes

---
+++ android-fundamentals-apps/DroidCafeInput/app/src/main/java/com/example/droidcafeinput/OrderActivity.java
@@ -0,0 +1,23 @@
+package com.example.droidcafeinput;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        textView = findViewById(R.id.order_textview);
+        Intent intent = getIntent();
+        String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        textView.setText(message);
+    }
+}
