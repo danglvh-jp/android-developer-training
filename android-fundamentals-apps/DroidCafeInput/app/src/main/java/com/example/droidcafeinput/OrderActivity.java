8dd458bde99d959b63d12bcd7d64dfc370e137a3
danglvh1004 <lvhd.hdlv@gmail.com>
2021-04-02 10:46:28 +0700

    Task 2: Use radio buttons

--- android-fundamentals-apps/DroidCafeInput/app/src/main/java/com/example/droidcafeinput/OrderActivity.java
+++ android-fundamentals-apps/DroidCafeInput/app/src/main/java/com/example/droidcafeinput/OrderActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -19,5 +22,35 @@ public class OrderActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         textView.setText(message);
+    }
+
+    public void displayToast(String messsage) {
+        Toast.makeText(getApplicationContext(), messsage, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked.
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    // Same day service
+                    displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    // Next day delivery
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    // Pick up
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                // Do nothing.
+                break;
+        }
     }
 }
