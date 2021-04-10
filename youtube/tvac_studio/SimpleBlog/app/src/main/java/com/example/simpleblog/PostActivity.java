d208d0e1ba7c2266246d7d5c5097fe6db809e32a
danglvh1004 <lvhd.hdlv@gmail.com>
2021-04-10 17:33:43 +0700

    Commit Part 4

--- youtube/tvac_studio/SimpleBlog/app/src/main/java/com/example/simpleblog/PostActivity.java
+++ youtube/tvac_studio/SimpleBlog/app/src/main/java/com/example/simpleblog/PostActivity.java
@@ -3,24 +3,52 @@ package com.example.simpleblog;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class PostActivity extends AppCompatActivity {
 
     private ImageButton mSelectImage;
+    private EditText mPostTitle;
+    private EditText mPostDesc;
+
+    private Button mSubmitBtn;
+
+    private Uri mImageUri = null;
 
     private static final int GALLERY_REQUEST = 1;
+
+    private StorageReference mStorage;
+
+    private ProgressDialog mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
+        mStorage = FirebaseStorage.getInstance().getReference();
+
         mSelectImage = findViewById(R.id.imageSelect);
+
+        mPostTitle = findViewById(R.id.titleField);
+        mPostDesc = findViewById(R.id.descField);
+
+        mSubmitBtn = findViewById(R.id.submitBtn);
+
+        mProgress = new ProgressDialog(this);
 
         mSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +59,42 @@ public class PostActivity extends AppCompatActivity {
                 startActivityForResult(galleryIntent, GALLERY_REQUEST);
             }
         });
+
+        mSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startPosting();
+            }
+        });
+    }
+
+    public void startPosting() {
+
+        mProgress.setMessage("Posting to Blog ...");
+        mProgress.show();
+
+        String title_val = mPostTitle.getText().toString().trim();
+        String desc_val = mPostDesc.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mImageUri != null) {
+
+            StorageReference filepath = mStorage.child("Blog_Images").child(mImageUri.getLastPathSegment());
+
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            final Uri downloadUrl = uri;
+                            mProgress.dismiss();
+                        }
+                    });
+                }
+            });
+        }
     }
 
     @Override
@@ -40,9 +104,9 @@ public class PostActivity extends AppCompatActivity {
         if (requestCode == GALLERY_REQUEST) {
             if (resultCode == RESULT_OK) {
 
-                Uri imageUri = data.getData();
+                mImageUri = data.getData();
 
-                mSelectImage.setImageURI(imageUri);
+                mSelectImage.setImageURI(mImageUri);
             }
         }
     }
