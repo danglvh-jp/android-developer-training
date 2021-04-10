be32d222fc2557841745a801e8700874d2ee51ae
danglvh1004 <lvhd.hdlv@gmail.com>
2021-04-10 16:30:59 +0700

    Commit Part 3

--- youtube/tvac_studio/SimpleBlog/app/src/main/java/com/example/simpleblog/PostActivity.java
+++ youtube/tvac_studio/SimpleBlog/app/src/main/java/com/example/simpleblog/PostActivity.java
@@ -1,14 +1,49 @@
 package com.example.simpleblog;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
+
+    private ImageButton mSelectImage;
+
+    private static final int GALLERY_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mSelectImage = findViewById(R.id.imageSelect);
+
+        mSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST) {
+            if (resultCode == RESULT_OK) {
+
+                Uri imageUri = data.getData();
+
+                mSelectImage.setImageURI(imageUri);
+            }
+        }
     }
 }
