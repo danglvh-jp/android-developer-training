3c7631e83174d7171cc638c2c7522b9af8c6ff59
danglvh1004 <lvhd.hdlv@gmail.com>
2021-04-10 16:12:31 +0700

    Commit Part 2

--- youtube/tvac_studio/SimpleBlog/app/src/main/java/com/example/simpleblog/MainActivity.java
+++ youtube/tvac_studio/SimpleBlog/app/src/main/java/com/example/simpleblog/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.simpleblog;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +18,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.action_add) {
+            startActivity(new Intent(MainActivity.this, PostActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
