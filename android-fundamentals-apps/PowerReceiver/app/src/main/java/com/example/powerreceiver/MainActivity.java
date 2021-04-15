4bf877d4127676e8b02f3a30e10d631940728eea
danglvh1004 <lvhd.hdlv@gmail.com>
2021-04-15 13:07:13 +0700

    Task 2. Send and receive a custom broadcast

--- android-fundamentals-apps/PowerReceiver/app/src/main/java/com/example/powerreceiver/MainActivity.java
+++ android-fundamentals-apps/PowerReceiver/app/src/main/java/com/example/powerreceiver/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.powerreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private CustomReceiver mReceiver = new CustomReceiver();
+
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +30,30 @@ public class MainActivity extends AppCompatActivity {
         // Register the receiver using the activity context, passing in the
         // IntentFilter.
         this.registerReceiver(mReceiver, filter);
+
+        // Register the receiver to receive custom broadcast.
+        LocalBroadcastManager.getInstance(this).registerReceiver
+                (mReceiver, new IntentFilter(ACTION_CUSTOM_BROADCAST));
     }
 
+    /**
+     * Unregisters the broadcast receivers when the app is destroyed.
+     */
     @Override
     protected void onDestroy() {
-        //Unregister the receiver
+        // Unregister the receivers.
         this.unregisterReceiver(mReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
         super.onDestroy();
+    }
+
+    /**
+     * Click event handler for the button, that sends custom broadcast using the
+     * LocalBroadcastManager.
+     */
+    public void sendCustomBroadcast(View view) {
+        Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+        LocalBroadcastManager.getInstance(this)
+                .sendBroadcast(customBroadcastIntent);
     }
 }
