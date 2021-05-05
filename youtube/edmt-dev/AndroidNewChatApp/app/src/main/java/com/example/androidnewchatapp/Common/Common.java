eaea7b076db6e0010dde0c99d106407351fbede6
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-05 16:39:21 +0700

    Commit

---
+++ youtube/edmt-dev/AndroidNewChatApp/app/src/main/java/com/example/androidnewchatapp/Common/Common.java
@@ -0,0 +1,8 @@
+package com.example.androidnewchatapp.Common;
+
+import com.example.androidnewchatapp.Model.UserModel;
+
+public class Common {
+    public static UserModel currentUser = new UserModel();
+    public static final String USER_REFERENCES = "People";
+}
---
+++ youtube/edmt-dev/AndroidNewChatApp/app/src/main/java/com/example/androidnewchatapp/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.androidnewchatapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.androidnewchatapp.Common.Common;
+import com.example.androidnewchatapp.Model.UserModel;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static int LOGIN_REQUEST_CODE = 7171;
+    private List<AuthUI.IdpConfig> providers;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+
+    FirebaseDatabase database;
+    DatabaseReference userRef;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(listener);
+    }
+
+    @Override
+    protected void onStop() {
+        if ((firebaseAuth != null) && (listener != null)) {
+            firebaseAuth.removeAuthStateListener(listener);
+        }
+        super.onStop();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        providers = Arrays.asList(new AuthUI.IdpConfig.PhoneBuilder().build());
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        database = FirebaseDatabase.getInstance();
+        userRef = database.getReference(Common.USER_REFERENCES);
+
+        listener = myFirebaseAuth -> {
+            Dexter.withContext(this).withPermissions(Arrays.asList(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)).withListener(new MultiplePermissionsListener() {
+                @Override
+                public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                    if (multiplePermissionsReport.areAllPermissionsGranted()) {
+                        FirebaseUser user = myFirebaseAuth.getCurrentUser();
+                        if (user != null) {
+                            checkUserFromFirebase();
+                        } else {
+                            showLoginLayout();
+                        }
+                    } else {
+                        displayToast("Please enable all permission");
+                    }
+                }
+
+                @Override
+                public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+
+                }
+            }).check();
+        };
+    }
+
+    private void checkUserFromFirebase() {
+        userRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull @org.jetbrains.annotations.NotNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            UserModel userModel = snapshot.getValue(UserModel.class);
+                            userModel.setUid(snapshot.getKey());
+                            gotoHomeActivity(userModel);
+                        } else {
+                            showRegisterLayout();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull @org.jetbrains.annotations.NotNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+    private void gotoHomeActivity(UserModel userModel) {
+        Common.currentUser = userModel;
+        startActivity(new Intent(MainActivity.this, HomeActivity.class));
+    }
+
+    private void showRegisterLayout() {
+        startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == LOGIN_REQUEST_CODE) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            } else {
+                displayToast("[ERROR]" + response.getError());
+            }
+
+        }
+    }
+
+    private void showLoginLayout() {
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+                .setIsSmartLockEnabled(false)
+                .setTheme(R.style.LoginTheme)
+                .setAvailableProviders(providers).build(), LOGIN_REQUEST_CODE);
+    }
+
+    private void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
