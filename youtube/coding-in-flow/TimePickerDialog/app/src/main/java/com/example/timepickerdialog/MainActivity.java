a0a425ce11fa1725b44e3751db9d1dfaf6af66bf
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-07 21:12:28 +0700

    Commit

---
+++ youtube/coding-in-flow/TimePickerDialog/app/src/main/java/com/example/timepickerdialog/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.timepickerdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        textView.setText("Hour: " + hourOfDay + " Minute: " + minute);
+    }
+}
