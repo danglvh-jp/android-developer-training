ae65254bedaf756d0557f2d5e439c519146852a7
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-09 15:28:54 +0700

    Commit

---
+++ youtube/coding-in-flow/SlidrExample/app/src/main/java/com/example/slidrexample/Activity2.java
@@ -0,0 +1,30 @@
+package com.example.slidrexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.r0adkll.slidr.Slidr;
+import com.r0adkll.slidr.model.SlidrInterface;
+
+public class Activity2 extends AppCompatActivity {
+
+    private SlidrInterface slidr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        slidr = Slidr.attach(this);
+    }
+
+    public void lockSlide(View view) {
+        slidr.lock();
+    }
+
+    public void unlockSlide(View view) {
+        slidr.unlock();
+    }
+}
