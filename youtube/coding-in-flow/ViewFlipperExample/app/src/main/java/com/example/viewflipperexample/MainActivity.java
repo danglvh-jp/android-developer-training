15330c99d5b2e55c4e331c2ea04db90cbca2a4f2
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-10 15:13:34 +0700

    Commit

---
+++ youtube/coding-in-flow/ViewFlipperExample/app/src/main/java/com/example/viewflipperexample/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.viewflipperexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewFlipper viewFlipper;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewFlipper = findViewById(R.id.view_flipper);
+        textView = new TextView(this);
+        textView.setText("Dynamically added TextView");
+        textView.setGravity(Gravity.CENTER);
+
+        viewFlipper.addView(textView);
+    }
+
+    public void previousView(View view) {
+        viewFlipper.setInAnimation(this, R.anim.slide_in_right);
+        viewFlipper.setOutAnimation(this, R.anim.slide_out_left);
+        viewFlipper.showPrevious();
+    }
+
+    public void nextView(View view) {
+        viewFlipper.setInAnimation(this, android.R.anim.slide_in_left);
+        viewFlipper.setOutAnimation(this, android.R.anim.slide_out_right);
+        viewFlipper.showNext();
+    }
+}
