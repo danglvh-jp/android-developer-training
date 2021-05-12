1c7cc417a567b3152690ea857a133d9b884e90a1
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-12 10:48:23 +0700

    Commit

---
+++ youtube/coding-in-flow/ToolbarExample/app/src/main/java/com/example/toolbarexample/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.toolbarexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
