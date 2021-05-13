d266f3da0dcdc88a5d76acd927bfcf1049c089e0
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-13 20:40:59 +0700

    Commit

---
+++ youtube/coding-in-flow/TextWatcherExample/app/src/main/java/com/example/textwatcherexample/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.textwatcherexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonConfirm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextUsername = findViewById(R.id.edit_text_username);
+        editTextPassword = findViewById(R.id.edit_text_password);
+        buttonConfirm = findViewById(R.id.button_confirm);
+
+        editTextUsername.addTextChangedListener(loginWatcher);
+        editTextPassword.addTextChangedListener(loginWatcher);
+    }
+
+    private TextWatcher loginWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String usernameInput = editTextUsername.getText().toString().trim();
+            String passwordInput = editTextUsername.getText().toString().trim();
+
+            buttonConfirm.setEnabled(!usernameInput.isEmpty() && !passwordInput.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+}
