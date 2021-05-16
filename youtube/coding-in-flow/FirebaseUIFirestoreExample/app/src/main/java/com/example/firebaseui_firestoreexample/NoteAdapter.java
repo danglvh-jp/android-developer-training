e1305e28011d02d64f27f93659b74601166d23be
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-16 11:02:35 +0700

    Commit Part 3

---
+++ youtube/coding-in-flow/FirebaseUIFirestoreExample/app/src/main/java/com/example/firebaseui_firestoreexample/NoteAdapter.java
@@ -0,0 +1,57 @@
+package com.example.firebaseui_firestoreexample;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+import org.jetbrains.annotations.NotNull;
+
+public class NoteAdapter extends FirestoreRecyclerAdapter<Note, NoteAdapter.NoteHolder> {
+
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
+     * FirestoreRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public NoteAdapter(@NonNull @NotNull FirestoreRecyclerOptions<Note> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull @NotNull NoteAdapter.NoteHolder holder, int position, @NonNull @NotNull Note model) {
+        holder.textViewTitle.setText(model.getTitle());
+        holder.textViewDescription.setText(model.getDescription());
+        holder.textViewPriority.setText(String.valueOf(model.getPriority()));
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false);
+        return new NoteHolder(view);
+    }
+
+    public class NoteHolder extends RecyclerView.ViewHolder {
+
+        private TextView textViewTitle;
+        private TextView textViewDescription;
+        private TextView textViewPriority;
+
+        public NoteHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+
+            textViewTitle = itemView.findViewById(R.id.text_view_title);
+            textViewDescription = itemView.findViewById(R.id.text_view_description);
+            textViewPriority = itemView.findViewById(R.id.text_view_priority);
+        }
+    }
+}
