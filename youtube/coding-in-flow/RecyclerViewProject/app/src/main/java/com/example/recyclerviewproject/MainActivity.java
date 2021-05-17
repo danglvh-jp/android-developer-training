7d4f3fefbab58398f9dabfe98d129a77e47cf89f
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-17 09:42:03 +0700

    Commit Part 1

---
+++ youtube/coding-in-flow/RecyclerViewProject/app/src/main/java/com/example/recyclerviewproject/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.recyclerviewproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<ExampleItem> exampleList = new ArrayList<>();
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 1", "Line 2"));
+        exampleList.add(new ExampleItem(R.drawable.ic_audio, "Line 3", "Line 4"));
+        exampleList.add(new ExampleItem(R.drawable.ic_sun, "Line 5", "Line 6"));
+    }
+}
