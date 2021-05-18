ea854e06873c69ec3db52d1413bbc5f654f6cf71
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-18 21:38:17 +0700

    Commit

---
+++ youtube/coding-in-flow/PicassorExample/app/src/main/java/com/example/picassorexample/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.picassorexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image_view);
+
+        String url = "https://cdn.pixabay.com/photo/2017/11/06/18/39/apple-2924531_960_720.jpg";
+        Picasso.get().load(url).into(imageView);
+    }
+}
