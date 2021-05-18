e07ec499de524af1a387b3ad2cafa1fd2e73a4a6
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-18 09:52:40 +0700

    Commit Part 3

--- youtube/coding-in-flow/RecyclerViewProject/app/src/main/java/com/example/recyclerviewproject/MainActivity.java
+++ youtube/coding-in-flow/RecyclerViewProject/app/src/main/java/com/example/recyclerviewproject/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
@@ -13,35 +16,66 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<ExampleItem> mExampleList;
+    private Button buttonInsert;
+    private Button buttonRemove;
+    private EditText editTextInsert;
+    private EditText editTextRemove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<ExampleItem> exampleList = new ArrayList<>();
-        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 1", "Line 2"));
-        exampleList.add(new ExampleItem(R.drawable.ic_audio, "Line 3", "Line 4"));
-        exampleList.add(new ExampleItem(R.drawable.ic_sun, "Line 5", "Line 6"));
-        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 7", "Line 8"));
-        exampleList.add(new ExampleItem(R.drawable.ic_audio, "Line 9", "Line 10"));
-        exampleList.add(new ExampleItem(R.drawable.ic_sun, "Line 11", "Line 12"));
-        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 13", "Line 14"));
-        exampleList.add(new ExampleItem(R.drawable.ic_audio, "Line 15", "Line 16"));
-        exampleList.add(new ExampleItem(R.drawable.ic_sun, "Line 17", "Line 18"));
-        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 19", "Line 20"));
-        exampleList.add(new ExampleItem(R.drawable.ic_audio, "Line 21", "Line 22"));
-        exampleList.add(new ExampleItem(R.drawable.ic_sun, "Line 23", "Line 24"));
-        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 25", "Line 26"));
-        exampleList.add(new ExampleItem(R.drawable.ic_audio, "Line 27", "Line 28"));
-        exampleList.add(new ExampleItem(R.drawable.ic_sun, "Line 29", "Line 30"));
+        createExampleList();
+        buildRecyclerView();
 
+        buttonInsert = findViewById(R.id.button_insert);
+        buttonRemove = findViewById(R.id.button_remove);
+        editTextInsert = findViewById(R.id.edittext_insert);
+        editTextRemove = findViewById(R.id.edittext_remove);
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextInsert.getText().toString());
+                insertItem(position);
+            }
+        });
+
+        buttonRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextRemove.getText().toString());
+                removeItem(position);
+            }
+        });
+    }
+
+    public void createExampleList() {
+        mExampleList = new ArrayList<>();
+        mExampleList.add(new ExampleItem(R.drawable.ic_android, "Line 1", "Line 2"));
+        mExampleList.add(new ExampleItem(R.drawable.ic_audio, "Line 3", "Line 4"));
+        mExampleList.add(new ExampleItem(R.drawable.ic_sun, "Line 5", "Line 6"));
+    }
+
+    public void buildRecyclerView() {
         mRecyclerView = findViewById(R.id.recyclerView);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new ExampleAdapter(exampleList);
+        mAdapter = new ExampleAdapter(mExampleList);
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    public void insertItem(int position) {
+        mExampleList.add(position, new ExampleItem(R.drawable.ic_android, "New Item At Postion " + position, "This is Line 2"));
+        mAdapter.notifyItemInserted(position);
+    }
+
+    public void removeItem(int position) {
+        mExampleList.remove(position);
+        mAdapter.notifyItemRemoved(position);
     }
 }
