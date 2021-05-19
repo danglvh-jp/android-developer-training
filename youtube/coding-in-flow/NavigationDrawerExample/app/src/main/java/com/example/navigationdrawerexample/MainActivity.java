62593ba2c6957d75007577164980d5b09821e97a
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-19 20:24:01 +0700

    Commit Part 2

--- youtube/coding-in-flow/NavigationDrawerExample/app/src/main/java/com/example/navigationdrawerexample/MainActivity.java
+++ youtube/coding-in-flow/NavigationDrawerExample/app/src/main/java/com/example/navigationdrawerexample/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.navigationdrawerexample;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Notification;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
