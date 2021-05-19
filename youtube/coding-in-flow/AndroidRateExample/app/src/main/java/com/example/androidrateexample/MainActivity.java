044cffc816bc304f4212617a70eb36ff2101e323
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-19 15:27:14 +0700

    Commit

---
+++ youtube/coding-in-flow/AndroidRateExample/app/src/main/java/com/example/androidrateexample/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.androidrateexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hotchemi.android.rate.AppRate;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AppRate.with(this)
+                .setInstallDays(1)
+                .setLaunchTimes(3)
+                .setRemindInterval(2)
+                .monitor();
+
+        AppRate.showRateDialogIfMeetsConditions(this);
+    }
+}
