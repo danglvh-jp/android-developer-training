7b1b9ab644022f155bf3569d48f1b116bee72f50
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-22 22:38:56 +0700

    Commit

---
+++ youtube/coding-in-flow/CustomToast/app/src/main/java/com/example/customtoast/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.customtoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView toastText;
+    private ImageView toastImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast();
+            }
+        });
+    }
+
+    public void showToast() {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast_layout, (ViewGroup) findViewById(R.id.toast_root));
+
+        toastText = layout.findViewById(R.id.toast_text);
+        toastImage = layout.findViewById(R.id.toast_image);
+
+        toastText.setText("This is a different text");
+        toastImage.setImageResource(R.drawable.ic_toasticon);
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+
+        toast.show();
+    }
+}
