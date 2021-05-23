de5467eab8354da8b40f610f3505f139349aeb13
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-23 22:56:51 +0700

    Commit RecycleView Android Example 03 Intents and navigation

--- youtube/shad-sluiter/PresidentListRecyclerView/app/src/main/java/com/example/presidentlistrecyclerview/AddEditOne.java
+++ youtube/shad-sluiter/PresidentListRecyclerView/app/src/main/java/com/example/presidentlistrecyclerview/AddEditOne.java
@@ -2,13 +2,38 @@ package com.example.presidentlistrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddEditOne extends AppCompatActivity {
+
+    private Button btnOk;
+    private Button btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_one);
+
+        btnOk = findViewById(R.id.btn_ok);
+        btnCancel = findViewById(R.id.btn_cancel);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddEditOne.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddEditOne.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- youtube/shad-sluiter/PresidentListRecyclerView/app/src/main/java/com/example/presidentlistrecyclerview/MainActivity.java
+++ youtube/shad-sluiter/PresidentListRecyclerView/app/src/main/java/com/example/presidentlistrecyclerview/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.presidentlistrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAddOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAddOne = findViewById(R.id.btn_addOne);
+
+        btnAddOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddEditOne.class);
+                startActivity(intent);
+            }
+        });
     }
 }
