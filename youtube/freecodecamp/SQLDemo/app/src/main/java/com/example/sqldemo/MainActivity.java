1b769e7490e37f3672d9342b4c8c4ccbe6866d1a
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-23 11:22:59 +0700

    Commit Part 3: Button Click Listeners

--- youtube/freecodecamp/SQLDemo/app/src/main/java/com/example/sqldemo/MainActivity.java
+++ youtube/freecodecamp/SQLDemo/app/src/main/java/com/example/sqldemo/MainActivity.java
@@ -3,12 +3,56 @@ package com.example.sqldemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAdd;
+    private Button btnViewAll;
+    private EditText edtName;
+    private EditText edtAge;
+    private Switch swActiveCustomer;
+    ListView lvCustomerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btn_add);
+        btnViewAll = findViewById(R.id.btn_viewAll);
+        edtAge = findViewById(R.id.et_age);
+        edtName = findViewById(R.id.et_name);
+        swActiveCustomer = findViewById(R.id.sw_active);
+        lvCustomerList = findViewById(R.id.lv_customerList);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    CustomerModel customerModel = new CustomerModel(-1, edtName.getText().toString(), Integer.parseInt(edtAge.getText().toString()), swActiveCustomer.isChecked());
+                    displayToast(customerModel.toString());
+                } catch (Exception e) {
+                    displayToast("Error creating customer");
+                }
+                displayToast("Add button");
+            }
+        });
+
+        btnViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayToast("View All button");
+            }
+        });
+    }
+
+    private void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
