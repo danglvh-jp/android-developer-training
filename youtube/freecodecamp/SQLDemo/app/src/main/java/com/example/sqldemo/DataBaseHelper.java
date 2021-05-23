f378d3169c70c5a64d7eda7a0faf544429adbd12
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-23 14:51:28 +0700

    Commit Part 4: SQLite Open Helper Class

---
+++ youtube/freecodecamp/SQLDemo/app/src/main/java/com/example/sqldemo/DataBaseHelper.java
@@ -0,0 +1,28 @@
+package com.example.sqldemo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "customer.db";
+
+    public DataBaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    // this is called the first time a database is accessed. There should be code in here to create a new database.
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    // this is called if the database version number changes. It prevents previous users apps from breaking when you change the database design.
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
