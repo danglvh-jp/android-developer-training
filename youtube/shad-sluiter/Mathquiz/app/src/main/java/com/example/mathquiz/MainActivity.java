048d43b51ade5a25d385059ff60142bfc579afc1
danglvh1004 <lvhd.hdlv@gmail.com>
2021-05-26 15:44:51 +0700

    Commit Java Android app math quiz 05 How to name controls

--- youtube/shad-sluiter/Mathquiz/app/src/main/java/com/example/mathquiz/MainActivity.java
+++ youtube/shad-sluiter/Mathquiz/app/src/main/java/com/example/mathquiz/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.mathquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnStart;
+    private Button btnAnswer0;
+    private Button btnAnswer1;
+    private Button btnAnswer2;
+    private Button btnAnswer3;
+    private TextView tvScore;
+    private TextView tvQuestions;
+    private TextView tvTimer;
+    private TextView tvBottomMessage;
+    private ProgressBar progTimer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btn_start);
+        btnAnswer0 = findViewById(R.id.btn_answer0);
+        btnAnswer1 = findViewById(R.id.btn_answer1);
+        btnAnswer2 = findViewById(R.id.btn_answer2);
+        btnAnswer3 = findViewById(R.id.btn_answer3);
+        tvScore = findViewById(R.id.tv_score);
+        tvBottomMessage = findViewById(R.id.tv_bottommessage);
+        tvQuestions = findViewById(R.id.tv_questions);
+        tvTimer = findViewById(R.id.tv_timer);
+        progTimer = findViewById(R.id.prog_timer);
+
+        tvTimer.setText("0Sec");
+        tvQuestions.setText("");
+        tvBottomMessage.setText("Press Go");
+        tvScore.setText("0pts");
     }
 }
