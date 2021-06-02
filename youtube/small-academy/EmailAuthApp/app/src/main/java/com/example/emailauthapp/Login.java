97d0b0502d7abc0be02796b582c92724ea9eec3f
danglvh1004 <dang.lvh.1004@gmail.com>
2021-06-02 10:20:09 +0700

    Commit Part 4: Register Layout Design

--- youtube/small-academy/EmailAuthApp/app/src/main/java/com/example/emailauthapp/Login.java
+++ youtube/small-academy/EmailAuthApp/app/src/main/java/com/example/emailauthapp/Login.java
@@ -2,13 +2,27 @@ package com.example.emailauthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button btnCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnCreateAccount = findViewById(R.id.btn_create_account);
+
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+            }
+        });
     }
 }
