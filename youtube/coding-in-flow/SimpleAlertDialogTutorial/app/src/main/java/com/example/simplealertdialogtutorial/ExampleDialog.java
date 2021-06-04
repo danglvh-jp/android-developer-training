da0f6374530bafab8537d36e6a5607a73c767e5a
danglvh1004 <dang.lvh.1004@gmail.com>
2021-06-04 15:05:55 +0700

    Commit Simple Dialog with 1 Button - Android Studio Tutorial

---
+++ youtube/coding-in-flow/SimpleAlertDialogTutorial/app/src/main/java/com/example/simplealertdialogtutorial/ExampleDialog.java
@@ -0,0 +1,30 @@
+package com.example.simplealertdialogtutorial;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class ExampleDialog extends AppCompatDialogFragment {
+
+    @NonNull
+    @org.jetbrains.annotations.NotNull
+    @Override
+    public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Information")
+                .setMessage("This is a Dialog")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+        return builder.create();
+    }
+}
---
+++ youtube/coding-in-flow/SimpleDialog/app/src/main/java/com/example/simpledialog/ExampleDialog.java
@@ -0,0 +1,2 @@
+package com.example.simpledialog;public class ExampleDialog {
+}
