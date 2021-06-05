6b752ac20f219f3d188c5867ca5a67ed8daebbb2
danglvh1004 <dang.lvh.1004@gmail.com>
2021-06-05 15:12:26 +0700

    Commit MyVideosLibrary Part 2

--- youtube/small-academy/MyVideosLibrary/app/src/main/java/com/example/myvideoslibrary/MainActivity.java
+++ youtube/small-academy/MyVideosLibrary/app/src/main/java/com/example/myvideoslibrary/MainActivity.java
@@ -5,9 +5,22 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "TAG";
     private RecyclerView rcvVideoList;
     private VideoAdapter videoAdapter;
 
@@ -18,7 +31,52 @@ public class MainActivity extends AppCompatActivity {
 
         rcvVideoList = findViewById(R.id.rcv_video_list);
         rcvVideoList.setLayoutManager(new LinearLayoutManager(this));
-        videoAdapter = new VideoAdapter();
+        videoAdapter = new VideoAdapter();/**/
         rcvVideoList.setAdapter(videoAdapter);
+
+        getJsonData();
+    }
+
+    private void getJsonData() {
+        String url = "https://raw.githubusercontent.com/bikashthapa01/myvideos-android-app/master/data.json";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+//                Log.d(TAG, "onResponse: " + response);
+                try {
+                    JSONArray categories = response.getJSONArray("categories");
+                    JSONObject categoriesData = categories.getJSONObject(0);
+                    JSONArray videos = categoriesData.getJSONArray("videos");
+
+//                    Log.d(TAG, "onResponse: " + videos);
+
+                    for (int i = 0; i < videos.length(); i++) {
+                        JSONObject video = videos.getJSONObject(i);
+//                        Log.d(TAG, "onResponse: " + video.getString("title"));
+
+                        Video v = new Video();
+
+                        v.setTitle(video.getString("title"));
+                        v.setDescription(video.getString("description"));
+                        v.setAuthor(video.getString("subtitle"));
+                        v.setImageUrl(video.getString("thumb"));
+                        JSONArray videoUrl = video.getJSONArray("sources");
+                        v.setVideoUrl(videoUrl.getString(0));
+
+                        Log.d(TAG, "onResponse: " + v.getVideoUrl());
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onErrorResponse: " + error.getMessage());
+            }
+        });
+
+        requestQueue.add(objectRequest);
     }
 }
